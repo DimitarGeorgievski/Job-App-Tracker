15b0b2fb16e94572a79c1287420125918ea7e32f
DimitarGeorgievski <muzipuzi123@gmail.com>
2026-05-12 01:31:46 +0200

    feat(auth): finished register, login and forgot-password pages

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -12,6 +12,8 @@ import { type Response } from 'express';
 import { CredentialsDto } from './dto/Credentials.dto';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
 import { createCompanyDto } from './dto/create-company.dto';
+import { type FastifyReply } from 'fastify';
+import '@fastify/cookie';
 
 @Controller('auth')
 export class AuthController {
@@ -28,33 +30,28 @@ export class AuthController {
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  async loginUser(@Body() credentials: CredentialsDto, @Res() res: Response) {
+  async loginUser(
+    @Body() credentials: CredentialsDto,
+    @Res() res: FastifyReply,
+  ) {
     const { user, token, refreshToken } =
       await this.authService.loginUser(credentials);
-      res.cookie('access-token', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: "lax",
-        maxAge: 1000 * 60 * 60
-      })
-      res.cookie('refresh-token', refreshToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: "lax",
-        maxAge: 1000 * 60 * 60 * 24 * 7
-      })
+    const isProd = process.env.NODE_ENV === 'production';
+    res.header('Set-Cookie', [
+      `access-token=${token}; HttpOnly; SameSite=Lax; Max-Age=${60 * 60}; Path=/${isProd ? '; Secure' : ''}`,
+      `refresh-token=${refreshToken}; HttpOnly; SameSite=Lax; Max-Age=${60 * 60 * 24 * 7}; Path=/${isProd ? '; Secure' : ''}`,
+    ]);
     res.send(user);
   }
 
   @HttpCode(HttpStatus.NO_CONTENT)
   @Post('refresh-token')
   async refreshAccessToken(
-    @Res() res: Response,
+    @Res() res: FastifyReply,
     @Headers('refresh-token') refreshToken: string,
   ) {
     const { token } = await this.authService.refreshAccessToken(refreshToken);
     res.header('access-token', token);
-    res.sendStatus(HttpStatus.NO_CONTENT);
   }
 
   @HttpCode(HttpStatus.NO_CONTENT)
--- server/src/main.ts
+++ server/src/main.ts
@@ -25,10 +25,11 @@ async function bootstrap() {
   });
   app.setGlobalPrefix('/app');
   app.enableCors({
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
     origin: 'http://localhost:3001',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    exposedHeaders: ['Set-Cookie'],
   });
   await app.listen(3000, '0.0.0.0');
 }
